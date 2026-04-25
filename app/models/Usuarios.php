1719352dc7f5e6ee90b3462c216ac5de6e93e757
Jhonattan <jhonattanlobos21@gmail.com>
2026-04-25 11:07:48 -0400

    Modelo usuario con metodo crear

--- app/models/Usuarios.php
+++ app/models/Usuarios.php
@@ -1,2 +1,34 @@
 <?php
-// modelo usuario (en construcción)
+
+class Usuario {
+    private $conn ;
+    private $table = "usuarios";
+
+    public $nombre;
+    public $email;
+    public $password;
+    public $direccion;
+
+    public function __construct($db){
+        $this->conn = $db;
+    }
+
+    public function crear(){
+        $query = "INSERT INTO " . $this->table . "
+        (nombre, email, password, direccion)
+        VALUES (:nombre, :email, :password, :direccion)";
+
+        $stmt = $this->conn->prepare($query);
+
+        //ENCRIPTAR CONTRASEÑA 
+        $password_hash = password_hash($this->password, PASSWORD_DEFAULT);
+
+        $stmt->bindParam(":nombre", $this->nombre);
+        $stmt->bindParam(":email", $this->email);
+        $stmt->bindParam(":password", $password_hash);
+        $stmt->bindParam(":direccion", $this->direccion); 
+
+        return $stmt->execute();
+    }
+
+}
